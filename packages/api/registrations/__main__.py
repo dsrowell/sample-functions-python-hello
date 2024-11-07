f9dd435e8d6ef0193e54401351c81b777d371708
David Rowell <david.rowell@siemens.com>
2024-11-07 12:31:44 -0500

    step

--- packages/api/registrations/__main__.py
+++ packages/api/registrations/__main__.py
@@ -1,14 +1,16 @@
 from util import add
 from dynamodb import DynamoDBManager
+
 def main(args):
     #name = args.get("name", "stranger")
     #sum = add(2, 2)
     #greeting = "Hello " + name + "! 2 + 2 = " + str(sum)
     #print(greeting)
     #return {"body": greeting}
-    try:
-        db = DynamoDBManager()
-        regs = db.get_all_registrations()
-    except Exception as e:
-        regs = e
+    #try:
+    #    db = DynamoDBManager()
+    #    regs = db.get_all_registrations()
+    #except Exception as e:
+    #    regs = e
+    regs = "none"
     return {"body": {"registrations": regs}}
