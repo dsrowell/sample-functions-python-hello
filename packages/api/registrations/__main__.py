a4456758fe1a0cc8e3d5ac17f9bf5248f149b94e
David Rowell <david.rowell@siemens.com>
2024-11-07 12:12:09 -0500

    regs

--- packages/api/registrations/__main__.py
+++ packages/api/registrations/__main__.py
@@ -1,7 +1,11 @@
 from util import add
+from dynamodb import DynamoDBManager
 def main(args):
-      name = args.get("name", "stranger")
-      sum = add(2, 2)
-      greeting = "Hello " + name + "! 2 + 2 = " + str(sum)
-      print(greeting)
-      return {"body": greeting}
+      db = DynamoDBManager()
+      regs = db.get_all_registrations()
+      #name = args.get("name", "stranger")
+      #sum = add(2, 2)
+      #greeting = "Hello " + name + "! 2 + 2 = " + str(sum)
+      #print(greeting)
+      #return {"body": greeting}
+      return {"body": {"registrations": regs}}
