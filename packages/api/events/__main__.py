ff8f3dcc822093e38cd9fc595041dd829cc69851
David <dsrowell@gmail.com>
2024-11-07 19:42:30 -0500

    step

--- packages/api/events/__main__.py
+++ packages/api/events/__main__.py
@@ -1,6 +1,2 @@
-from util import add
-def main(args):
-      name = args.get("name", "stranger")
-      greeting = "Hello " + name + "!"
-      print(greeting)
-      return {"body": greeting}
+def main(event, context):
+    return {"body": {"event": event}}
