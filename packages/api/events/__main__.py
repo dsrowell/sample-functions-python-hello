d312c93618e6944d33d8d0272571c80beba66723
David Rowell <dsrowell@gmail.com>
2025-01-06 20:48:05 -0500

    work

--- packages/api/events/__main__.py
+++ packages/api/events/__main__.py
@@ -12,9 +12,12 @@ def main(event, context):
     method = http['method']
     path = http['path']
 
+    print(f'event: {event}')
     year = event['year'] if 'year' in event else None
+    print(f'year: {year}')
 
     if year:
+        print('creating manager')
         db = DynamoDBManager()
         items = db.get_all_events(year)
         return {"body": {"event": event, "items": items}}
